d072467d41b6337e3b6ad02748ddcc9886333abd
Chris Smith-Clarke <chrissmithclarke@gmail.com>
2023-04-04 04:11:06 +0100

    add summary routes and tests

--- app/models/tortoise.py
+++ app/models/tortoise.py
@@ -1,4 +1,6 @@
 from tortoise import fields, models
+from tortoise.contrib.pydantic.creator import pydantic_model_creator
+from pydantic import BaseModel
 
 
 class TextSummary(models.Model):
@@ -8,3 +10,5 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+SummarySchema = pydantic_model_creator(TextSummary)
